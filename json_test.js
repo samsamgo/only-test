2bdd3a0987e125ed9e9bee5790810ebd922947b6
samsamgo <jku2659@gmail.com>
2022-10-21 12:06:03 +0900

    serialize

---
+++ json_test.js
@@ -0,0 +1,14 @@
+/*직렬화*/
+const message = {
+    sender: "김코딩",
+    receiver: "박해커",
+    message: "해커야 오늘 저녁 같이 먹을래?",
+    createdAt: "2021-01-12 10:10:10"
+}
+
+let transferableMessage = JSON.stringify(message)
+
+console.log(transferableMessage)
+// `{"sender":"김코딩","receiver":"박해커","message":"해커야 오늘 저녁 같이 먹을래?","createdAt":"2021-01-12 10:10:10"}`
+
+console.log(typeof (transferableMessage))
