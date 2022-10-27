965c67a29d60b80d7d688c170219e5758ef46dc8
samsamgo <jku2659@gmail.com>
2022-10-27 17:31:48 +0900

    react  Styled Components
    make globalstyle by use styled-components

--- modal/my-app/src/App.js
+++ modal/my-app/src/App.js
@@ -1,6 +1,16 @@
 //import styles from "./App.css"
 import styled from "styled-components"
+import { createGlobalStyle } from "styled-components";
 //import { RiCloseLine } from "react-icons/ri"
+
+const GlobalStyle = createGlobalStyle`
+  button{
+    padding : 5px;
+    margin : 2px;
+    border-radius : 5px
+  }
+`
+
 
 const Button1 = styled.button`
   background : ${(props) => (props.color ? props.color : "white")};
@@ -12,9 +22,10 @@ export default function App() {
   // React 컴포넌트를 사용하듯이 사용하면 됩니다.
   return (
     <>
+      <GlobalStyle />
       <Button1>Button</Button1>
       <Button1 color="orange">Button1</Button1>
-      <Button color="skyblue">Button1</Button>
+      <Button1 color="skyblue">Button1</Button1>
     </>
   );
 }
