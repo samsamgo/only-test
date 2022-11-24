d596bc85e1dbed3c070b6efe3e4aa8afad8d8332
samsamgo <jku2659@gmail.com>
2022-11-24 22:54:24 +0900

    webpack_test

--- fe-sprint-webpack-tutorial/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'), // './dist'의 절대 경로를 리턴합니다.
-        filename: 'app.bundle.js',
-    },
-};
