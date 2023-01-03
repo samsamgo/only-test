01bff4636876c9433bbebcc83fdc48c82f97050b
samsamgo <jku2659@gmail.com>
2023-01-04 01:24:37 +0900

    json-server && axios

--- axios/axios/src/App.js
+++ axios/axios/src/App.js
@@ -1,7 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/test")
+      .then(function (response) {
+        // response
+        console.log(response.data); //데이터 전송 성공시
+      })
+      .catch(function (error) {
+        // 오류발생시 실행
+      })
+      .then(function () {
+        // 항상 실행
+      }); //컴포넌트가 리랜더링 될때마다 실행
+  });
+
   return (
     <div className="App">
       <header className="App-header">
